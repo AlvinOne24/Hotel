d1867e2b7dfaf230f5305c8124036a88f3a80444
AlvinOne24 <al24vin@mhs.mdp.ac.id>
2022-06-17 09:31:04 +0700

    Update show.blade.php

--- reservasihotel/resources/views/laboum/show.blade.php
+++ reservasihotel/resources/views/laboum/show.blade.php
@@ -6,7 +6,7 @@
         <div class="col">
             <h2>Profil Pelanggan {{ $laboum->nama}}</h2>
             <div class="d-md-flex justify-content-md-end">
-            <a href="{{ url('laboum')}}" class="btn btn-primary mt-2">kembali</a>
+            <a href="{{ url('laboum')}}" class="btn btn-primary mt-2">Back</a>
             </div>
             <table class="table table-striped table-hover">
                 <tr>
